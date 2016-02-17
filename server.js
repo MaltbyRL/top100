a703b818f4e9fb26c4c3f65dda4314501ea3bd2f
MaltbyRL <RLMaltby@gmail.com>
2016-02-17 15:13:12 -0800

    commiting

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var express = require('express');
+var stormpath = require('express-stormpath')
 
 var api = require('./routes/api');
 
 var app = express();
 
+app.use(stormpath.init(app, { website: true}));
+
 app.use('/api', api);
 
-app.listen(process.env.PORT || 3000)
+
+app.on('stormpath.ready', () => {
+  app.listen(process.env.PORT || 3000)
+})
 
 
 
